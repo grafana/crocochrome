10b2c9b003ae904002246bd993e1a7389a24fc86
Nadia Santalla <nadia@santalla.io>
2024-10-21 14:51:49 +0200

    manualtest/croco6: allow using an out-of-path k6 binary

--- manualtest/croco6.sh
+++ manualtest/croco6.sh
@@ -8,6 +8,7 @@ set -eo pipefail
 # By default this assumes you're already running crocochrome in docker, e.g.:
 # docker run --ti --rm -p 8080:8080 -p 5222:5222 localhost:5000/crocochrome
 CROCOCHROME_URL=${CROCOCHROME_URL:-http://localhost:8080}
+K6=${K6:-k6}
 
 session=$(curl -sSL "$CROCOCHROME_URL"/sessions -d '{}')
 id=$(echo "$session" | jq -r .id)
@@ -15,13 +16,13 @@ echo "Got session $id"
 
 url=$(echo "$session" | jq -r .chromiumVersion.webSocketDebuggerUrl)
 if [[ -z $url ]]; then
-	echo "Crocochrome returned an empty url"
-	exit 1
+  echo "Crocochrome returned an empty url"
+  exit 1
 fi
 
 echo "Browser URL: $url"
 
-K6_BROWSER_WS_URL="$url" k6 "$@" || true
+K6_BROWSER_WS_URL="$url" "$K6" "$@" || true
 
 echo "Deleting session $id"
 curl -sSL -X DELETE "$CROCOCHROME_URL/sessions/$id"
