06cf732d3205190b5a91e4595224b453080e306c
Nadia Santalla <nadia@santalla.io>
2024-10-22 10:21:02 +0200

    main: compute user agent on start

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log/slog"
 	"net/http"
 	"os"
@@ -34,6 +35,13 @@ func main() {
 		ExtraUATerms: "GrafanaSyntheticMonitoring",
 	})
 
+	err := supervisor.ComputeUserAgent(context.Background())
+	if err != nil {
+		logger.Error("Computing user agent", "err", err)
+		os.Exit(1)
+		return
+	}
+
 	server := crocohttp.New(logger, supervisor)
 	instrumentedServer := metrics.InstrumentHTTP(registry, server)
 
@@ -43,7 +51,7 @@ func main() {
 	const address = ":8080"
 	logger.Info("Starting HTTP server", "address", address)
 
-	err := http.ListenAndServe(address, mux)
+	err = http.ListenAndServe(address, mux)
 	if err != nil {
 		logger.Error("Setting up HTTP listener", "err", err)
 	}
