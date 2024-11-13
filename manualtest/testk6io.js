842b712f4a2da041a97858dc1a3aec793e58279c
Nadia Santalla <nadia@santalla.io>
2024-11-13 11:25:12 +0100

    manualtest: update simple script
    Copied from https://grafana.com/docs/k6/latest/using-k6-browser/

--- manualtest/testk6io.js
+++ manualtest/testk6io.js
@@ -1,8 +1,9 @@
-import { browser } from 'k6/experimental/browser';
+import { browser } from 'k6/browser';
+import { check } from 'https://jslib.k6.io/k6-utils/1.5.0/index.js';
 
 export const options = {
   scenarios: {
-    browser: {
+    ui: {
       executor: 'shared-iterations',
       options: {
         browser: {
@@ -17,13 +18,24 @@ export const options = {
 };
 
 export default async function () {
-  const page = browser.newPage();
+  const context = await browser.newContext();
+  const page = await context.newPage();
 
   try {
-    await page.goto('https://test.k6.io/');
-    console.log(page.locator('p[class="description"]').innerText());
-    page.screenshot("screenshot.png");
+    await page.goto("https://test.k6.io/my_messages.php");
+
+    await page.locator('input[name="login"]').type("admin");
+    await page.locator('input[name="password"]').type("123");
+
+    await Promise.all([
+      page.waitForNavigation(),
+      page.locator('input[type="submit"]').click(),
+    ]);
+
+    await check(page.locator("h2"), {
+      'header': async h2 => await h2.textContent() == "Welcome, admin!"
+    });
   } finally {
-    page.close();
+    await page.close();
   }
 }
