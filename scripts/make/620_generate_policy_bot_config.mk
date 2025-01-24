d77eb61a8d1dfec9bd83b7b1390543dd69e1b091
Nadia Santalla <nadia@santalla.io>
2025-01-24 10:19:10 +0100

    Revert "feat: add policy bot configuration (#112)"
    This reverts commit a45d434c5a18db176666a3e69458dc0c787179ee.

--- scripts/make/620_generate_policy_bot_config.mk
+++
@@ -1,5 +0,0 @@
-.PHONY: generate-policy-bot-config
-generate-policy-bot-config: ## Generate policy bot config.
-	$(S) echo 'Generating policy bot configuration...'
-	$(V) $(ROOTDIR)/scripts/gen-policy-bot-config "$(ROOTDIR)"
-	$(S) echo 'Done.'
